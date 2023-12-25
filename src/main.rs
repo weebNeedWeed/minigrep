04cdb5f874e8223848f5b3905f0c47849809d898
weebNeedWeed <rivenmle@gmail.com>
2023-12-25 20:54:57 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use minigrep::{self, Config};
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
